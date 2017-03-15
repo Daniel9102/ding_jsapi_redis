3f1efbca9164d4dcdf5e13e078017f2e57419e9d
Nico_Zhang <2091571762@qq.com>
2017-03-15 16:01:19 +0800

    first

---
+++ Readme.md
@@ -0,0 +1,63 @@
+# ding_jsapi_redis
+> 钉钉Node.js SDK,基于redis缓存access_token
+
+### 开始
+首先安装npm包
+```
+npm install --save ding_jsapi_redis
+```
+
+### 引用
+config.js
+```
+//dingTalk基本配置，其中prefix为自定义的前缀，方便在redis中区分
+exports.ding = {
+	corpId:"<INPUT YOUR DINGTALK CORPID>",
+    secret:"<INPUT YOUR DINGTALK CORPSECRET>",
+    prefix:"<INPUT YOUR PREFIX>"
+};
+exports.redis = {
+	port:6379,
+    host :'192.168.1.218',
+    auth:{}
+};
+```
+在需要调用的地方
+```
+var ding = require("ding_jsapi_redis");
+var config = require(PATH+"/config");
+//传入参数
+ding.conf.ding = config.ding;
+ding.conf.redis = config.redis;
+
+```
+### 使用
+通用get请求
+```
+//通用get请求
+ding.httpGetFunc({
+	action:"department/get",
+	params:{}
+}).then()
+```
+参数说明：
+dingTalk中一般的get请求方式为：
+https://oapi.dingtalk.com/department/list?access_token=ACCESS_TOKEN
+其中，https://oapi.dingtalk.com为通用部分，故在这里统一封装，在调用时直接将department/list作为调用的方法名以参数形式传入，access_token为实际参数
+action:string
+params:{} 为可选，默认为{}
+
+### 举个栗子
+dingTalk文档中
+```
+https://oapi.dingtalk.com/department/get?access_token=ACCESS_TOKEN&id=2
+```
+获取部门详情 department/get
+实际调用时
+```
+ding.httpGetFunc({
+	action:"department/get",
+	params:{id:1}
+}).then()
+```
+
---
+++ index.js
@@ -0,0 +1,115 @@
+/**
+ * Created by nico on 2017/3/11.
+ */
+var Q = require("q");
+var async = require("async");
+var auth = require("./lib/auth");
+var config = require("./config")
+module.exports = {
+    conf: config,
+    //获取token
+    getAccessToken:function(){
+        var q = Q.defer();
+        auth.getAccessToken().then(function (data) {
+            q.resolve(data);
+        }).catch(function (err) {
+            q.reject(err);
+        })
+        return q.promise;
+    },
+    //get公用方法
+    httpGetFunc:function (args) {
+        var q = Q.defer();
+        async.waterfall([
+            function (cb) {
+                auth.getAccessToken().then(function (data) {
+                    cb(null,data);
+                }).catch(function (err) {
+                    cb(err)
+                });
+            },
+            function (token,cb) {
+                var params = args.params || {} ;
+                params["access_token"] = token[config.ding.prefix].token ;
+                auth.commonHttpGet({
+                    action:args.action,
+                    params:params
+                }).then(function(data){
+                    cb(null,data);
+                }).catch(function (err) {
+                    cb(err);
+                })
+            }
+        ],function (err,result) {
+            if(err){
+                q.reject(err);
+            }else{
+                q.resolve(result);
+            }
+        })
+        return q.promise;
+    },
+    //公共post请求方法
+    httpPostFunc:function (args) {
+        var q = Q.defer();
+        async.waterfall([
+            function (cb) {
+                auth.getAccessToken().then(function (data) {
+                    cb(null,data);
+                }).catch(function (err) {
+                    cb(err)
+                });
+            },
+            function (token,cb) {
+                var params = args.params || {} ;
+                // params["access_token"] = token[config.ding.prefix].token ;
+                auth.commonHttpPost({
+                    action:args.action,
+                    access_token :token[config.ding.prefix].token ,
+                    params:params
+                }).then(function(data){
+                    cb(null,data);
+                }).catch(function (err) {
+                    cb(err);
+                })
+            }
+        ],function (err,result) {
+            if(err){
+                q.reject(err);
+            }else{
+                q.resolve(result);
+            }
+        })
+        return q.promise;
+    },
+    //获取部门列表
+    getDepartmentList:function () {
+        var q = Q.defer();
+        async.waterfall([
+            function (cb) {
+                auth.getAccessToken().then(function (data) {
+                    cb(null,data);
+                }).catch(function (err) {
+                    cb(err)
+                });
+            },
+            function (token,cb) {
+                auth.commonHttpGet({action:"department/list",params:{access_token:token[config.ding.prefix].token}}).then(function(data){
+                    cb(null,data);
+                }).catch(function (err) {
+                    cb(err);
+                })
+            }
+        ],function (err,result) {
+            if(err){
+                q.reject(err);
+            }else{
+                q.resolve(result);
+            }
+        })
+        return q.promise;
+    },
+    getSignature:function(){
+
+    },
+}
